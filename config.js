e3f632de740f5ba7b4c3d1ae85d87e475d5fe201
SRIRAMDADI1 <sd19022007@gmail.com>
2021-11-21 20:43:48 +0530

    first commit

--- config.js
+++ config.js
@@ -2,7 +2,12 @@ import firebase from 'firebase';
 
 // add SDK Firebase
 var firebaseConfig = {
-
+  apiKey: "AIzaSyAKt9C5ZBYzQP2rT3MayG8P1jLLLtXqvY4",
+  authDomain: "project67-e5106.firebaseapp.com",
+  projectId: "project67-e5106",
+  storageBucket: "project67-e5106.appspot.com",
+  messagingSenderId: "684162870172",
+  appId: "1:684162870172:web:8560bdb2cca59b6a16f659"
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
